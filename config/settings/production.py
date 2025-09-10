28b6980821370af413492cbcaae3faa314524aa3
Sistoconstru <consgtrusisto@gmail.com>
2025-09-10 15:20:11 -0500

    para railway

--- config/settings/production.py
+++ config/settings/production.py
@@ -36,3 +36,15 @@ LOGOUT_REDIRECT_URL = '/auth/login/'
 DATABASES = {
     'default': dj_database_url.config(default=config('DATABASE_URL'))
 }
+
+INSTALLED_APPS += ['storages']
+
+# Configuración de AWS S3
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME', 'us-east-1')
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
