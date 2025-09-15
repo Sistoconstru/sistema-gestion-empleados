9a01e903fce8ee0d3f74ceee71fc95b784e2a080
Sistoconstru <consgtrusisto@gmail.com>
2025-09-15 07:40:56 -0500

    Elimina archivos de base de datos local del repositorio2

--- start.sh
+++ start.sh
@@ -5,13 +5,6 @@ set -e
 # Ejecuta migraciones
 python manage.py migrate
 
-# Crea superusuario automáticamente si no existe
-if [ "$DJANGO_SUPERUSER_USERNAME" ] && [ "$DJANGO_SUPERUSER_EMAIL" ] && [ "$DJANGO_SUPERUSER_PASSWORD" ]; then
-	python manage.py createsuperuser \
-		--noinput \
-		--username "$DJANGO_SUPERUSER_USERNAME" \
-		--email "$DJANGO_SUPERUSER_EMAIL" || true
-fi
 
 # Inicia Gunicorn en modo producción
 exec gunicorn config.wsgi:application --bind 0.0.0.0:8000
