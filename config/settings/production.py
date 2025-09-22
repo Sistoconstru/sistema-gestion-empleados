d527e4a9cdcc67136b1408811a5502e61abf75f4
Sistoconstru <consgtrusisto@gmail.com>
2025-09-22 16:05:59 -0500

    s3 permiso acceso

--- config/settings/production.py
+++ config/settings/production.py
@@ -61,7 +61,7 @@ AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME', 'sa-east-1')  # Ejemplo: 'sa-east-1' para São Paulo
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
-AWS_QUERYSTRING_AUTH = False
+AWS_QUERYSTRING_AUTH = True
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
