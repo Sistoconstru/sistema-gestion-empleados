f332ccdfbe27a768b66f7c1559e7b254d004c4fd
Sistoconstru <consgtrusisto@gmail.com>
2025-09-26 11:33:46 -0500

    edicion en start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 set -e
 
+export DJANGO_SETTINGS_MODULE=config.settings.production
 
 # Ejecuta migraciones
 python manage.py migrate
 
 # Crea un superusuario si no existe
-
 
 # Inicia Gunicorn en modo producción
 exec gunicorn config.wsgi:application --bind 0.0.0.0:8000
