e6dad89c7217240c549ed1895fc4a769ba09631f
Sistoconstru <construsisto@gmail.com>
2025-11-10 08:24:37 -0500

    Fix: Corregir error de sintaxis en production.py y agregar storage explicito para insignias y beneficios

--- config/settings/production.py
+++ config/settings/production.py
@@ -3,7 +3,7 @@ from .base import *
 # Configuración de storage para producción
 DEFAULT_FILE_STORAGE = 'custom_storage.media.MediaStorage'
 
-car el settings activo
+# Log para verificar el settings activo
 import os
 import logging
 from django.conf import settings
